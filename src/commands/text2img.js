425c0103d72f073272e05c2b674c123888a5735a
Jay Joo <jayjoo0727@gmail.com>
2025-01-19 17:26:14 -0800

    Removed batch from t2img

--- src/commands/text2img.js
+++ src/commands/text2img.js
@@ -1,55 +1,47 @@
-import { SlashCommandBuilder } from "discord.js";
+import { SlashCommandBuilder } from 'discord.js';
 
 const text2img = new SlashCommandBuilder()
-	.setName("text2img")
-	.setDescription("Convert text to image")
+	.setName('text2img')
+	.setDescription('Convert text to image')
 	.addStringOption((option) =>
-		option.setName("prompt").setDescription("Text to convert").setRequired(true)
+		option.setName('prompt').setDescription('Text to convert').setRequired(true)
 	)
 	.addNumberOption((option) =>
 		option
-			.setName("width")
-			.setDescription("Width of the image")
+			.setName('width')
+			.setDescription('Width of the image')
 			.setRequired(false)
 			.setMinValue(128)
 			.setMaxValue(1024)
 	)
 	.addNumberOption((option) =>
 		option
-			.setName("height")
-			.setDescription("Height of the image")
+			.setName('height')
+			.setDescription('Height of the image')
 			.setRequired(false)
 			.setMinValue(128)
 			.setMaxValue(1024)
 	)
 	.addNumberOption((option) =>
 		option
-			.setName("steps")
-			.setDescription("Number of steps")
+			.setName('steps')
+			.setDescription('Number of steps')
 			.setRequired(false)
 			.setMinValue(5)
 			.setMaxValue(20)
 	)
 	.addNumberOption((option) =>
 		option
-			.setName("batch_count")
-			.setDescription("Batch count")
-			.setRequired(false)
-			.setMinValue(1)
-			.setMaxValue(4)
-	)
-	.addNumberOption((option) =>
-		option
-			.setName("batch_size")
-			.setDescription("Batch size")
+			.setName('batch_size')
+			.setDescription('Batch size')
 			.setRequired(false)
 			.setMinValue(1)
 			.setMaxValue(5)
 	)
 	.addBooleanOption((option) =>
 		option
-			.setName("enhance_prompt")
-			.setDescription("Enhance prompt")
+			.setName('enhance_prompt')
+			.setDescription('Enhance prompt')
 			.setRequired(false)
 	);
 
